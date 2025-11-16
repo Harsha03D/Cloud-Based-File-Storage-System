87e55b9f36a2c782c229d63e4bd284eb27b908ef
Harsha03D <harsha2k03@gmail.com>
2025-11-16 17:45:09 +0530

    awsConfig update

--- frontend/src/awsConfig.js
+++ frontend/src/awsConfig.js
@@ -1,8 +1,8 @@
 const awsConfig = {
-  region: process.env.REACT_APP_REGION,
-  userPoolId: process.env.REACT_APP_USER_POOL_ID,
-  clientId: process.env.REACT_APP_CLIENT_ID,
-  authDomain: process.env.REACT_APP_COGNITO_DOMAIN,
+  region: "eu-north-1",
+  userPoolId: "eu-north-1_URXU8H356",
+  clientId: "42r9hdkdbnbi51rihfobs731r9",
+  authDomain: "process.env.REACT_APP_COGNITO_DOMAIN",
 };
 
 export default awsConfig;
